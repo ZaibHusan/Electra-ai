9a0c5304bcb304f2b3f6722e21ef212974c7b841
Hussan Zaib <husanzaibilc@gmail.com>
2026-08-10 23:06:13 -0700

    Health is aded

--- backend/app-service/index.js
+++ backend/app-service/index.js
@@ -10,12 +10,18 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello, World! i am App Service');
-})
+  res.send('Hello, World! i am App Service');
+});
 
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'ok',
+    service: 'agent-service'
+  });
+});
 
-app.use('/api/webhook',webhookRoute)
+app.use('/api/webhook', webhookRoute);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
