63d3342cc72755a02bf6fb7e4b803d539a794607
Hussan Zaib <husanzaibilc@gmail.com>
2026-08-15 03:51:22 -0700

    Fix api: Remove fallback URL for BACKEND_URL in api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 const api = axios.create({
     baseURL: BACKEND_URL,
     withCredentials: true,
