4188338aeaaec73b080d260a2d4286fad4023462
Hussan Zaib <husanzaibilc@gmail.com>
2026-08-15 03:41:51 -0700

    Fix socket service: Set default SOCKET_URL to fallback address if environment variable is not defined

--- frontend/src/services/socket.service.js
+++ frontend/src/services/socket.service.js
@@ -1,7 +1,6 @@
 // src/services/socket.service.js
 import { io } from "socket.io-client";
-
-const SOCKET_URL = import.meta.env.VITE_BACKEND_URL;
+const SOCKET_URL = import.meta.env.VITE_BACKEND_URL || "http://82.180.133.170:4000";
 
 export const socket = io(SOCKET_URL, {
   autoConnect: true,
